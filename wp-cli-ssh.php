77ccdd3330a0dca9b955fb7630465f5afb18547f
Weston Ruter <weston@x-team.com>
2013-11-20 14:31:46 -0800

    Rework wp-cli-ssh.php to be loadable via Composer

--- wp-cli-ssh.php
+++ wp-cli-ssh.php
@@ -26,113 +26,138 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-/**
- * Gather SSH config from global and project-specific config files
- */
-$ssh_config = array();
-foreach ( array( $this->global_config_path, $this->project_config_path ) as $config_path ) {
-	$config = self::load_config( $config_path );
-	if ( ! empty( $config['ssh'] ) ) {
-		$ssh_config = array_merge( $ssh_config, $config['ssh'] );
-	}
-}
-if ( empty( $ssh_config ) ) {
-	return;
-}
+call_user_func(
+	function( $runner ) {
 
-// Parse cli args to push to server
-$has_url       = false;
-$path          = null;
-$target_server = null;
-$cli_args      = array();
-
-// @todo Better to use WP_CLI::get_configurator()->parse_args() here?
-foreach ( array_slice( $GLOBALS['argv'], 1 ) as $arg ) {
-	if ( preg_match( '#^--ssh-host=(.+)$#', $arg, $matches ) ) {
-		$target_server = $matches[1];
-	}
-	else if ( preg_match( '#^--path=(.+)$#', $arg, $matches ) ) {
-		$path = $matches[1];
-	}
-	else {
-		if ( preg_match( '#^--url=#', $arg ) ) {
-			$has_url = true;
+		/**
+		 * This script can either be supplied via a WP-CLI --require config, or
+		 * itcan be loaded via a Composer package. In the latter case:
+		 * Bootstrap this file to be required inside of WP_CLI\Runner::before_wp_load()
+		 * so that it will be run even before do_early_invoke, and so that we have
+		 * access to $this and the private variables and methods, such as:
+		 * - WP_CLI\Runner::find_command_to_run()
+		 * - WP_CLI\Runner::$config
+		 * - WP_CLI\Runner::$global_config_path
+		 * - WP_CLI\Runner::$project_config_path
+		 * YES, the result is that this file is required twice. YES, this is hacky!
+		 */
+		$require_arg = sprintf( '--require=%s', __FILE__ );
+		if ( empty( $runner ) ) {
+			$GLOBALS['argv'][] = $require_arg;
+			return;
 		}
-		$cli_args[] = $arg;
-	}
-}
 
-// Check if a target is specified or fallback on local if not.
-if ( $target_server && ! isset( $ssh_config[$target_server] ) ){
-	WP_CLI::error( "The target host you specified doesn't exist in the config file." );
-} else if ( ! $target_server ) {
-	return;
-}
+		/**
+		 * Gather SSH config from global and project-specific config files
+		 */
+		$ssh_config = array();
+		foreach ( array( $runner->global_config_path, $runner->project_config_path ) as $config_path ) {
+			$config = self::load_config( $config_path );
+			if ( ! empty( $config['ssh'] ) ) {
+				$ssh_config = array_merge( $ssh_config, $config['ssh'] );
+			}
+		}
 
-// Revert back to default host if not explicitly specified
-$target_server = ( $target_server ) ?: $ssh_config['default'];
+		// Parse cli args to push to server
+		$has_url       = false;
+		$path          = null;
+		$target_server = null;
+		$cli_args      = array();
 
-// Check if the currently runned command is disabled on remote server
-// Also check if we have a valid command
-if ( isset( $ssh_config[$target_server]['disabled_commands'] ) ){
-	$this->config['disabled_commands'] = $ssh_config[$target_server]['disabled_commands'];
-}
+		// @todo Better to use WP_CLI::get_configurator()->parse_args() here?
+		foreach ( array_slice( $GLOBALS['argv'], 1 ) as $arg ) {
+			// Remove what we added above the first time this file was loaded
+			if ( $arg === $require_arg ) {
+				continue;
+			} else if ( preg_match( '#^--ssh-host=(.+)$#', $arg, $matches ) ) {
+				$target_server = $matches[1];
+			} else if ( preg_match( '#^--path=(.+)$#', $arg, $matches ) ) {
+				$path = $matches[1];
+			} else {
+				if ( preg_match( '#^--url=#', $arg ) ) {
+					$has_url = true;
+				}
+				$cli_args[] = $arg;
+			}
+		}
 
-// Check if command is valid or disabled **from core wp-cli
-$r = $this->find_command_to_run( array_values( $cli_args ) );
-if ( is_string( $r ) ) {
-	WP_CLI::error( $r );
-}
+		// Check if a target is specified or fallback on local if not.
+		if ( $target_server && ! isset( $ssh_config[$target_server] ) ){
+			WP_CLI::error( "The target SSH host you specified doesn't exist in the wp-cli config file." );
+		} else if ( ! $target_server ) {
+			return;
+		}
 
-// Add default url from config is one is not set
-if ( ! $has_url && ! empty( $ssh_config[$target_server]['url'] ) ) {
-	$cli_args[] = '--url=' . $ssh_config[$target_server]['url'];
-}
+		// Revert back to default host if not explicitly specified
+		$target_server = ( $target_server ) ?: $ssh_config['default'];
 
-if ( ! $path && ! empty( $ssh_config[$target_server]['path'] ) ) {
-	$path = $ssh_config[$target_server]['path'];
-} else {
-	WP_CLI::error( 'No path is specified' );
-}
+		// Check if the currently runned command is disabled on remote server
+		// Also check if we have a valid command
+		if ( isset( $ssh_config[$target_server]['disabled_commands'] ) ){
+			$runner->config['disabled_commands'] = $ssh_config[$target_server]['disabled_commands'];
+		}
 
-// Inline bash script
-$cmd = <<<'BASH'
-if command -v wp >/dev/null 2>&1; then
-    wp_command=wp;
-else
-    wp_command=/tmp/wp-cli.phar;
-    if [ ! -e $wp_command ]; then
-        curl -L https://github.com/wp-cli/builds/blob/gh-pages/phar/wp-cli.phar?raw=true > $wp_command;
-        chmod +x $wp_command;
-    fi;
-fi;
-cd %s;
-$wp_command
-BASH;
+		// Check if command is valid or disabled **from core wp-cli
+		$r = $runner->find_command_to_run( array_values( $cli_args ) );
+		if ( is_string( $r ) ) {
+			WP_CLI::error( $r );
+		}
 
-// Replace path
-$cmd = sprintf( $cmd, escapeshellarg( $path ) );
+		// Add default url from config is one is not set
+		if ( ! $has_url && ! empty( $ssh_config[$target_server]['url'] ) ) {
+			$cli_args[] = '--url=' . $ssh_config[$target_server]['url'];
+		}
 
-// Append WP-CLI args to command
-$cmd  = trim( preg_replace( '/\s+/', ' ', $cmd ) );
-$cmd .= ' ' . join( ' ', array_map( 'escapeshellarg', $cli_args ) );
+		if ( ! $path && ! empty( $ssh_config[$target_server]['path'] ) ) {
+			$path = $ssh_config[$target_server]['path'];
+		} else {
+			WP_CLI::error( 'No path is specified' );
+		}
 
-// Escape command argument for each level of SSH tunnel inception, and pass along TTY state
-$is_tty       = function_exists( 'posix_isatty' ) && posix_isatty( STDOUT );
-$cmd_prefix   = $ssh_config[$target_server]['cmd'];
-$cmd_prefix   = str_replace( '%pseudotty%', ( $is_tty ? '-t' : '-T' ), $cmd_prefix );
-$tunnel_depth = preg_match_all( '/(^|\s)(ssh|slogin)\s/', $cmd_prefix );
-for ( $i = 0; $i < $tunnel_depth; $i += 1 ) {
-	$cmd = escapeshellarg( $cmd );
-}
+		// Inline bash script
+		$cmd = '
+			if command -v wp >/dev/null 2>&1; then
+				wp_command=wp;
+			else
+				wp_command=/tmp/wp-cli.phar;
+				if [ ! -e $wp_command ]; then
+					curl -L https://github.com/wp-cli/builds/blob/gh-pages/phar/wp-cli.phar?raw=true > $wp_command;
+					chmod +x $wp_command;
+				fi;
+			fi;
+			cd %s;
+			$wp_command
+		';
 
-// Replace placeholder with command
-$cmd = str_replace( '%cmd%', $cmd, $cmd_prefix );
+		// Replace path
+		$cmd = sprintf( $cmd, escapeshellarg( $path ) );
 
-if ( ! $is_tty ) { // they probably want this to be --quiet
-	WP_CLI::log( sprintf( 'Connecting to remote host: %s', $target_server ) );
-}
+		// Remove newlines in Bash script added just for readability
+		$cmd  = trim( preg_replace( '/\s+/', ' ', $cmd ) );
 
-passthru( $cmd, $exit_code );
+		// Append WP-CLI args to command
+		$cmd .= ' ' . join( ' ', array_map( 'escapeshellarg', $cli_args ) );
 
-exit( $exit_code );
+		// Escape command argument for each level of SSH tunnel inception, and pass along TTY state
+		$is_tty       = function_exists( 'posix_isatty' ) && posix_isatty( STDOUT );
+		$cmd_prefix   = $ssh_config[$target_server]['cmd'];
+		$cmd_prefix   = str_replace( '%pseudotty%', ( $is_tty ? '-t' : '-T' ), $cmd_prefix );
+		$tunnel_depth = preg_match_all( '/(^|\s)(ssh|slogin)\s/', $cmd_prefix );
+		for ( $i = 0; $i < $tunnel_depth; $i += 1 ) {
+			$cmd = escapeshellarg( $cmd );
+		}
+
+		// Replace placeholder with command
+		$cmd = str_replace( '%cmd%', $cmd, $cmd_prefix );
+
+		if ( ! $is_tty ) { // they probably want this to be --quiet
+			WP_CLI::log( sprintf( 'Connecting to remote host: %s', $target_server ) );
+		}
+
+		passthru( $cmd, $exit_code );
+
+		exit( $exit_code );
+
+	},
+	empty( $this ) ? null : $this
+); // Hi, JavaScript!
